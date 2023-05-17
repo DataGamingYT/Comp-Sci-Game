7dc758537aac2ffd0832d30f3364d1198501d9fd
Data Gaming <rylanhughey@gmail.com>
2023-05-16 20:59:01 -0500

    Main.java was modified

--- Main.java
+++ Main.java
@@ -5,126 +5,86 @@ import java.awt.event.KeyListener;
 
 public class Main {
     public static void main(String[] args) {
-
-        
-
-
-        // Create the stage layout
-
-        int[][] stageLayout = {
-            {1, 1, 1, 1, 1},
-            {1, 1, 1, 1, 1},
-            {1, 0, 1, 0, 0},
-            {1, 0, 1, 0, 0},
-            {0, 0, 0, 0, 0}
-        };
-
-        SwingUtilities.invokeLater(() -> createAndShowGUI(stageLayout));
+        SwingUtilities.invokeLater(Main::createAndShowGUI);
     }
 
-    private static void createAndShowGUI(int[][] stageLayout) {
-        JFrame frame = new JFrame("Main Stage");
-    
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+    private static void createAndShowGUI() {
+    JFrame frame = new JFrame("Sprite Movement");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create the stage panel and set the layout manager
-        JPanel stagePanel = new JPanel();
-        stagePanel.setLayout(new GridLayout(stageLayout.length, stageLayout[0].length));
+    StagePanel stagePanel = new StagePanel();
+    stagePanel.setFocusable(true);
+    stagePanel.requestFocusInWindow(); // Request focus explicitly
 
-        // Populate the stage panel with tiles based on the layout
-        for (int[] row : stageLayout) {
-            for (int tile : row) {
-                JPanel tilePanel = new JPanel();
-                tilePanel.setBackground(tile == 1 ? Color.BLUE : Color.GRAY);
-                stagePanel.add(tilePanel);
-            }
-        }
-
-        // Create the sprite
-        Sprite sprite = new Sprite();
-
-        // Add the sprite to the stage panel
-        stagePanel.add(sprite);
-
-        // Add key listener to the frame
-        frame.addKeyListener(new KeyListener() {
-            @Override
-            public void keyTyped(KeyEvent e) {}
-
-            @Override
-            public void keyPressed(KeyEvent e) {
-                // Handle sprite movement based on arrow keys
-                int keyCode = e.getKeyCode();
-                switch (keyCode) {
-                    case KeyEvent.VK_UP:
-                        sprite.moveUp();
-                        break;
-                    case KeyEvent.VK_DOWN:
-                        sprite.moveDown();
-                        break;
-                    case KeyEvent.VK_LEFT:
-                        sprite.moveLeft();
-                        break;
-                    case KeyEvent.VK_RIGHT:
-                        sprite.moveRight();
-                        break;
-                }
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {}
-        });
-
-        // Set the focus on the frame to receive key events
-        frame.setFocusable(true);
-        frame.requestFocus();
-
-        frame.getContentPane().add(stagePanel);
-        frame.pack();
-        frame.setVisible(true);
-    }
+    frame.getContentPane().add(stagePanel);
+    frame.pack();
+    frame.setVisible(true);
 }
 
-class Sprite extends JPanel {
-    private int x;
-    private int y;
-    private static final int SIZE = 3;
-    private static final int MOVE_AMOUNT = 3;
+}
 
-    public Sprite() {
-        this.x = 0;
-        this.y = 0;
-        setPreferredSize(new Dimension(SIZE, SIZE));
-        setBackground(Color.RED);
+class StagePanel extends JPanel {
+    private static final int PANEL_WIDTH = 400;
+    private static final int PANEL_HEIGHT = 400;
+    private static final int SPRITE_SIZE = 50;
+    private static final int SPRITE_MOVE_AMOUNT = 10;
+
+    private int spriteX;
+    private int spriteY;
+
+    public StagePanel() {
+        setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        setBackground(Color.WHITE);
+
+        // Initial position of the sprite
+        spriteX = (PANEL_WIDTH - SPRITE_SIZE) / 2;
+        spriteY = (PANEL_HEIGHT - SPRITE_SIZE) / 2;
+
+        addKeyListener(new SpriteKeyListener());
     }
 
-    public void moveUp() {
-        if (y - MOVE_AMOUNT >= 0) {
-            y -= MOVE_AMOUNT;
-            setLocation(x, y);
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLUE);
+        g.fillRect(spriteX, spriteY, SPRITE_SIZE, SPRITE_SIZE);
+    }
+
+    private class SpriteKeyListener implements KeyListener {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_UP:
+                    if (spriteY - SPRITE_MOVE_AMOUNT >= 0) {
+                        spriteY -= SPRITE_MOVE_AMOUNT;
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (spriteY + SPRITE_MOVE_AMOUNT + SPRITE_SIZE <= PANEL_HEIGHT) {
+                        spriteY += SPRITE_MOVE_AMOUNT;
+                    }
+                    break;
+                case KeyEvent.VK_LEFT:
+                    if (spriteX - SPRITE_MOVE_AMOUNT >= 0) {
+                        spriteX -= SPRITE_MOVE_AMOUNT;
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (spriteX + SPRITE_MOVE_AMOUNT + SPRITE_SIZE <= PANEL_WIDTH) {
+                        spriteX += SPRITE_MOVE_AMOUNT;
+                    }
+                    break;
+            }
+            repaint(); // Redraw the sprite
         }
-    }
 
-    public void moveDown() {
-        if (y + MOVE_AMOUNT + SIZE <= getParent().getHeight()) {
-            y += MOVE_AMOUNT;
-            setLocation(x, y);
+        @Override
+        public void keyTyped(KeyEvent e) {
         }
-    }
 
-    public void moveLeft() {
-        if (x - MOVE_AMOUNT >= 0) {
-            x -= MOVE_AMOUNT;
-            setLocation(x, y);
-        }
-    }
-
-
-    public void moveRight() {
-        if (x + MOVE_AMOUNT + SIZE <= getParent().getWidth()) {
-            x += MOVE_AMOUNT;
-           
+        @Override
+        public void keyReleased(KeyEvent e) {
         }
     }
 }
